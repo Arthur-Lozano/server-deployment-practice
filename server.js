3e9a84d3cbcba7a4f522213a7cb63c72e430110b
Arthur Lozano <chillout885@gmail.com>
2021-02-20 18:59:22 -0800

    update

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ const notFoundHandler = require('./handlers/404.js');
 const errorHandler = require('./handlers/500.js');
 const stamper = require('./middleware/stamper.js');
 
+
 app.get('/', stamper, (req, res) => {
   res.status(200).send('Hello World')
 })
+
 
 app.get('/data', stamper, (req, res) => {
   let outputObject = {
@@ -20,6 +22,7 @@ app.get('/data', stamper, (req, res) => {
 
   res.status(200).json(outputObject);
 });
+
 
 app.get('/bad', (req, res, next) => {
   next('you messsed up')
@@ -32,6 +35,7 @@ app.use(errorHandler);
 function start(port) {
   app.listen(port, () => console.log(`Server up on port ${port}`))
 }
+
 
 module.exports = {
   app: app,
